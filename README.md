04a6fbdfec2381cdd984c35a00f4fb257854485c
hushicai <bluthcy@gmail.comm>
2021-05-07 09:28:39 +0800

    ADD web platform

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ app端基于react-native框架。
 
 web端基于react-native-web进行web化，本地开发使用`vitejs`，极致的开发体验。
 
+## web
+
+如果应用无需区分h5、pc，例如响应式的应用，则可以直接使用web。
+
+```
+npm run start:web
+```
+
 ## h5
 
 ```
--- typings/process.d.ts
+++ typings/process.d.ts
@@ -1,7 +1,7 @@
 declare const process: {
   env: {
     MODE: 'debug' | 'test' | 'production';
-    PLATFORM: 'H5' | 'App' | 'PC';
+    PLATFORM: 'Web' | 'H5' | 'App' | 'PC';
     HOST: string;
     NODE_ENV: 'development' | 'production';
   };
