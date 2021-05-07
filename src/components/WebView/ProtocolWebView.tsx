a12c31438b8f9225ad102fe6bf0e1fb1243d2d96
hushicai <bluthcy@gmail.comm>
2021-05-07 08:58:40 +0800

    remove comment

--- src/components/WebView/ProtocolWebView.tsx
+++ src/components/WebView/ProtocolWebView.tsx
@@ -13,7 +13,6 @@ export default function ProtocolWebView({
   const html = wrapIntoHtml(content);
   return (
     <WebView
-      // 如果 html.length 不足以区分，再考虑 MD5 摘要
       key={html.length}
       style={[styles.webview, style]}
       source={{html}}
